c2c39059bb0092f3f297c8a231791d02e241e73c
Andrew Chin <achin@eminence32.net>
2016-01-30 11:24:57 -0500

    Added regionset constructor

--- liboverviewer/src/world.rs
+++ liboverviewer/src/world.rs
@@ -75,12 +75,43 @@ impl Iterator for RegionsetIter {
 ///
 /// See the docs for the World object for more information on the difference
 /// between Worlds and RegionSets.
-pub struct Regionset;
+#[derive(Debug)]
+pub struct Regionset {
+    region_dir: PathBuf,
+    regions: Vec<(i32, i32)>
+}
 impl Regionset {
     /// Given a folder of MCA files, create a RegionSet
     pub fn new<P: AsRef<Path>>(p: P) -> Result<Regionset, OverviewerError> {
-        unimplemented!()
+        let region_dir = p.as_ref();
+        if !region_dir.exists() {
+            return Err(From::from(format!("Path {:?} does not exist", region_dir)))
+        }
+
+        let mut regions = Vec::new();
+        for file in try!(region_dir.read_dir()) {
+            let file = try!(file);
+            let file_name = file.file_name();
+            let fname_str = file_name.to_string_lossy();
+            let components: Vec<&str> = fname_str.split('.').collect();
+            if components.len() == 4 && components[0] == "r" && components[3] == "mca" {
+                let x = i32::from_str_radix(components[1], 10);
+                let z = i32::from_str_radix(components[2], 10);
+                if x.is_ok() && z.is_ok() {
+                    regions.push((x.unwrap(), z.unwrap())); 
+                }
+            }
+
+        }
+
+        Ok(Regionset{
+            region_dir: region_dir.to_owned(),
+            regions: regions
+        })
+
+
     }
+
     pub fn get_type(&self) -> String {
         unimplemented!()
     }
@@ -119,6 +150,12 @@ mod test {
     #[should_panic(expected = "IOError")]
     fn test_world_open_error() {
         let world = World::new("/").unwrap();
+    }
+
+    #[test]
+    fn test_regionset() {
+        let rset = Regionset::new("tests/data/OTD/world_189/region").unwrap();
+        assert_eq!(rset.regions.len(), 6);
     }
 
     #[test]
