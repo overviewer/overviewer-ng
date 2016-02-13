57f371c42a7ef0fc406d0c9b1fcec7151be477c1
Andrew Chin <achin@eminence32.net>
2016-02-13 13:35:40 -0500

    A better/faster height_map
    Instead of iterating over pixels and then finding the chunk that
    contained it, iterate over chunks+blocks, then find the pixel for that
    block.  The result is much faster, even in a debug build.

--- src/height_map.rs
+++ src/height_map.rs
@@ -15,32 +15,41 @@ fn main() {
 
     let mut chunk_cache: HashMap<(i64, i64), Chunk> = HashMap::new();
 
-    let imgx = 1024;
-    let imgy = 1024;
+    let imgx = 512;
+    let imgy = 512;
     let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
     let mut cur_x = 0;
 
-    for (x, z, pixel) in imgbuf.enumerate_pixels_mut() {
-        if z != cur_x {
-            cur_x = z;
-            //println!("{}", z);
+    // produce an imagemap for a single region, where each block column is 1 pixel.  so 512 by 512
+    for chunk_x in 0..32 {
+        for chunk_z in 0..32 {
+            let chunk_in_region = coords::Coord::new(chunk_x, 0, chunk_z);
+            if let Some(chunk) = rset.get_chunk(chunk_in_region) {
+                let map = chunk.get_heightmap();
+
+                for block_x in 0..16 {
+                    for block_z in 0..16 {
+                        let block_in_chunk = coords::Coord::<coords::Block,
+                                                             coords::Chunk>::new(block_x,
+                                                                                 0,
+                                                                                 block_z);
+                        let h = map.get((block_x + (block_z * 16)) as usize).unwrap();
+
+                        let block_in_region = block_in_chunk.join(chunk_in_region);
+                        assert!(block_in_region.x >= 0);
+                        assert!(block_in_region.z >= 0);
+                        // *pixel = image::Luma([255u8 - *h as u8 ]);
+                        imgbuf.put_pixel(block_in_region.x as u32,
+                                         block_in_region.z as u32,
+                                         image::Luma([255u8 - *h as u8]));
+                    }
+                }
+            }
+
         }
-        let block_coord = coords::Coord::<coords::Block, coords::World>::new(x as i64 - 512, 0, z as i64 - 512);
-        let (block_in_chunk, chunk_in_world) = block_coord.split::<coords::Chunk>();
-
-        if !rset.chunk_exists(chunk_in_world) {
-            continue;
-        }
-        let chunk: &Chunk = chunk_cache.entry((chunk_in_world.x, chunk_in_world.z)).or_insert_with(|| {
-            rset.get_chunk(chunk_in_world).unwrap()
-        });
-
-        let map = chunk.get_heightmap();
-
-        let h = map.get((block_in_chunk.x + (block_in_chunk.z*16)) as usize).unwrap();
-        *pixel = image::Luma([255u8 - *h as u8 ]);
-
     }
+
+
     let ref mut fout = File::create(&Path::new("hmap.png")).unwrap();
 
     // We must indicate the image’s color type and what format to save as
